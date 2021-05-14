468c35ad623d434ec98708ac66ed6ef79f2fe388
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-05-14 17:17:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition;
+function start(){
+    document.getElementById("text_box").innerHTML="";
+    recognition.start();
+}
+recognition.onresult=function run(event){
+    console.log(event);
+    var Content=event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("text_box").innerHTML=Content;
+    speak();
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("text_box").value;
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width: 360, 
+    height: 250,
+    image_format: 'png', 
+    png_quality: 90
+
+});
+camera=document.getElementById("camera");
